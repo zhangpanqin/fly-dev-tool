fa90bd508bc4ebd1bb3077ecbf05a1fe69c37b9e
zhangpanqin <zhangpanqin@outlook.com>
2022-04-19 23:14:03 +0800

    build(N/A): 修改项目依赖管理
    Signed-off-by: zhangpanqin <zhangpanqin@outlook.com>

--- java/spring-boot-demo/src/main/java/com/mflyyou/SpringBootDemoApplication.java
+++ java/spring-boot-demo/src/main/java/com/mflyyou/SpringBootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCacheApplication {
+public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCacheApplication.class, args);
+        SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
 }
